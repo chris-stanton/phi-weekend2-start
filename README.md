e129e34915a7ac720e67625006f712be3d551fff
Chris Stanton <chrisstanton@Chriss-MacBook-Pro.local>
2017-02-18 18:36:46 -0600

    button and appends completed and working

--- README.md
+++ README.md
@@ -5,3 +5,54 @@ This Challenge will require you to setup and run the application in this reposit
 [Refer to the jQuery AJAX lecture notes for installing and running the server]  (https://github.com/kdszafranski/prime-ajax-intro)
 
 These instructions will be the same for all of our node applications.
+
+------------------------------------------------------------------------------
+
+Assignment Details:
+
+https://github.com/kdszafranski/phi-weekend2-start
+
+Weekend Challenge 02:
+Welcome to your second weekend challenge! We're going to build a common web user interface app called a Carousel.
+
+Setup:
+Download or fork this repo.
+Open the repo in terminal and run npm install.
+
+TIPS:
+Don't forget, to test an application with a node server, you will need to enter npm start (as defined in the package.json file).
+To shut down a server, hold down control and press 'C'.
+
+Challenge Overview:
+I created a new JSON data file and inside it you will find an array of objects. Each object, is each one of you!
+
+On the DOM
+
+On the DOM should be:
+
+1. One person's information
+
+2. A series of 18 (or the number of people in the data array) index points with the first person's index highlighted or called out in style differently than the others.
+
+3. A 'Next' button and a 'Previous' button
+Clicking on the Next button should navigate to the next person, clicking on the Previous button should navigate to the previous person. The highlighted index point should update also as you click through to other people.
+Person Display
+
+4. When a person is displayed, show their name, their Github link, and their piece of shoutout feedback. Only one person should be showcased at any given time.
+
+tip:
+You will need to combine everything you learned this week to accomplish this task, and each of the challenges you have completed this week play a part in this task.
+
+
+Working Example:
+
+Here is a similar example from Zeta so you can see the functionality. It's really ugly, however. The code is also minified (no cheating!):
+
+https://polar-ravine-37299.herokuapp.com/
+
+HARD MODE:
+Include a fade out and fade in animation in-between transitioning people.
+
+
+PRO MODE:
+Include a timer that moves to the next person if the user is not clicking on next or prev. If the user clicks on next or prev, the timer should be reset. The timer should transition between people every 10 seconds.
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,12 +1,81 @@
-$(document).ready(function(){
 
-    // Upon page load, get the data from the server
+$(document).ready(function(){
+init();
+//variables
+var i = 0;
+var highStudentCount = 18;
+var lowStudentCount = -18;
+
+function init(){
+// Upon page load, get the data from the server
     $.ajax({
       type: "GET",
       url: "/data",
       success: function(data){
-        // yay! we have data!
-        console.log('returned data from server: ', data);
-      }
-    });
-});
+        //phirephiters = data;
+        console.log('data from server: ', data);
+        for (var i = 0; i < data.phirephiters.length; i++) {
+        }//end of FOR loop
+click();
+
+//info append to <div id="informationContainer"></div> on html
+        function appendInfo(phirephiters){
+          $("#informationContainer").append("<div class='phirephiters'></div>");
+          var $el = $("#informationContainer").children().last();
+          $el.append("<div><p>" + data.phirephiters[i].name + "</p></div>");
+          $el.append("<div><p>" + 'Git Username: ' + data.phirephiters[i].git_username + "</p></div>");
+          $el.append("<div><p>" + 'Shoutout: ' + data.phirephiters[i].shoutout + "</p></div>");
+        }//end of appendInfo()
+
+function click() {
+//next button
+          $("#nextButton").click(function(){
+            $(".phirephiters").remove();
+            i++;
+              if (i == highStudentCount){
+                i = 0;
+              }//end of IF
+
+              $("#slider").each(function() {
+                    if ($(this).data(i) == i) {
+                      $(this).css("background-color", "teal");
+                    } else {
+                      $(this).css("background-color", "white");
+                    }//end of IF
+              });//end of .each()
+appendInfo();
+            });//end of #nextButton on click function
+
+//previous button
+            $("#prevButton").click(function(){
+              $(".phirephiters").remove();
+              i--;
+                if (i === -1){
+                  i = highStudentCount;
+                }//end of IF
+
+                $("#slider").each(function() {
+                      if ($(this).data(i) == i) {
+                        $(this).css("background-color", "teal");
+                      } else {
+                        $(this).css("background-color", "white");
+                      }//end of IF
+                });//end of .each()
+appendInfo();
+            });//end of #prevButton on click function
+}//end of click()
+
+bottom();
+
+//appends bottom index
+        function bottom(phirephiters){
+          for (var i = 0; i < data.phirephiters.length; i++) {
+          $(".bottom").append('<div class="indexClass"></div>');
+          $(".bottom").children().last().data("name", i);
+          $(".bottom").children().first().css("background-color", "teal");
+          }//end of FOR
+        }//end of addBottomIndex()
+      }//end of sucess: function
+    });//end of ajax
+  }//end of loadData()
+});//end of document.ready
