21e48ca1d114ef8ad7b5b362482858be703d6c6b
Chris Stanton <chrisstanton@Chriss-MacBook-Pro.local>
2017-02-19 19:14:14 -0600

    base mode almost completed, need to fix slider

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 init();
 //variables
 var i = 0;
+var objectIndex = 0;
 var highStudentCount = 18;
 var lowStudentCount = -18;
 
@@ -12,15 +13,15 @@ function init(){
       type: "GET",
       url: "/data",
       success: function(data){
-        //phirephiters = data;
-        console.log('data from server: ', data);
         for (var i = 0; i < data.phirephiters.length; i++) {
         }//end of FOR loop
 click();
 
 //info append to <div id="informationContainer"></div> on html
-        function appendInfo(phirephiters){
+        function appendInfo(){
           $("#informationContainer").append("<div class='phirephiters'></div>");
+          //animation
+          $("#informationContainer").animate({opacity: "0"}, 300).animate({opacity: "1"}, 300);
           var $el = $("#informationContainer").children().last();
           $el.append("<div><p>" + data.phirephiters[i].name + "</p></div>");
           $el.append("<div><p>" + 'Git Username: ' + data.phirephiters[i].git_username + "</p></div>");
@@ -35,46 +36,38 @@ function click() {
               if (i == highStudentCount){
                 i = 0;
               }//end of IF
-
-              $("#slider").each(function() {
-                    if ($(this).data(i) == i) {
-                      $(this).css("background-color", "teal");
-                    } else {
-                      $(this).css("background-color", "white");
-                    }//end of IF
-              });//end of .each()
+console.log("next: " + i);
 appendInfo();
             });//end of #nextButton on click function
-
 //previous button
             $("#prevButton").click(function(){
               $(".phirephiters").remove();
               i--;
-                if (i === -1){
+                if (i == -1){
                   i = highStudentCount;
                 }//end of IF
-
-                $("#slider").each(function() {
-                      if ($(this).data(i) == i) {
-                        $(this).css("background-color", "teal");
-                      } else {
-                        $(this).css("background-color", "white");
-                      }//end of IF
-                });//end of .each()
+console.log("previous: " + i);
 appendInfo();
             });//end of #prevButton on click function
 }//end of click()
 
-bottom();
 
-//appends bottom index
-        function bottom(phirephiters){
-          for (var i = 0; i < data.phirephiters.length; i++) {
-          $(".bottom").append('<div class="indexClass"></div>');
-          $(".bottom").children().last().data("name", i);
-          $(".bottom").children().first().css("background-color", "teal");
-          }//end of FOR
-        }//end of addBottomIndex()
+
+
+// bottom();
+// //appends bottom index
+//         function bottom(){
+//           for (var j = 0; j < data.phirephiters.length; j++) {
+//           var $el = $(".bottom").children().last();
+//           $(".bottom").append("<div class='bottomIndex'>" + i + "</div>");
+//           $(".bottom").children().last().data("name", j);
+//           $(".bottom").children().first().css("background-color", "teal");
+//           }//end of FOR
+//         }//end of addBottomIndex()
+
+
+
+
       }//end of sucess: function
     });//end of ajax
   }//end of loadData()
